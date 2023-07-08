4541b44f17aecb2fa58feea489f7d0268e48f8d8
rushdanafis <130189960+rushdanafis@users.noreply.github.com>
2023-07-09 02:21:42 +0530

    birthday app

--- cash-app.js
+++ cash-app.js
@@ -6,29 +6,24 @@ let numberNotes = document.querySelectorAll(".number-notes");
 let notesArray = [2000, 500, 100, 50, 20, 10, 5, 1];
 
 checkButton.addEventListener("click", function validateMoney() {
+  errorMessege.style.display = "none";
 
-   errorMessege.style.display="none"
-   
- 
   if (billAmount.value > 0) {
-   
     if (cashGiven.value >= billAmount.value) {
-      
       let moneyReturn = cashGiven.value - billAmount.value;
-      console.log(moneyReturn)
+      console.log(moneyReturn);
       calculateMoney(moneyReturn);
     } else {
-      
+      errorMessege.style.display="block"
       errorMessege.innerText = "oops! seems someone forgot to bring money";
     }
   } else {
-    errorMessege.style.display="block"
+    errorMessege.style.display = "block";
     errorMessege.innerText = "enter solid money dude, number can't be negative";
   }
 });
 
 function calculateMoney(moneyReturn) {
- 
   for (let i = 0; i < notesArray.length; i++) {
     let numberOfnotes = Math.trunc(moneyReturn / notesArray[i]);
     moneyReturn = moneyReturn % notesArray[i];
