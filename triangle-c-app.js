73a20fe411c91ce9b1af03cdbbecbf653fa1a052
rushdanafis <130189960+rushdanafis@users.noreply.github.com>
2023-07-14 00:49:51 +0530

    calculate hypotenuse

---
+++ triangle-c-app.js
@@ -0,0 +1,11 @@
+let firstSide = document.querySelector("#first-side");
+let secondSide = document.querySelector("#second-side");
+let outPutMessege = document.querySelector(".output-messege");
+let checkBtn = document.querySelector("#check-hypotenuse");
+
+checkBtn.addEventListener("click", function calculateValues() {
+  let hypotenuse = Math.sqrt(
+    Math.pow(Number(firstSide.value), 2) + Math.pow(Number(secondSide.value), 2)
+  );
+  outPutMessege.innerText = ` The hypotenuse is ${hypotenuse}`;
+});
